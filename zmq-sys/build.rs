89f7b51b5f5f44f5eb75565d871361e8e0df4282
Jim McGrath <jimmc2@gmail.com>
2017-09-24 14:41:01 -0500

    let vcpkg determine the lib name

--- zmq-sys/build.rs
+++ zmq-sys/build.rs
@@ -49,46 +49,13 @@ fn try_vcpkg() -> bool {
 
 #[cfg(target_env="msvc")]
 fn try_vcpkg() -> bool {
-    match vcpkg::Config::new().lib_name("libzmq-mt-4_2_3").probe("zeromq") {
+    match vcpkg::Config::new().probe("zeromq") {
         Err(e) => {
             println!("vcpkg did not find zeromq: {}", e);
             false
         }
         Ok(lib_config) => {
             println!("cargo:rustc-link-lib=iphlpapi");
-
-            // // generated binding expects to link to a lib called "zmq.lib" but the
-            // // library that was found is "libzmq.lib", so make a copy as zmq.lib into
-            // // into OUT_DIR. It is not necessary to create a copy of the DLL because
-            // // the import library version of the .lib will try to load it as libzmq.dll
-            // if lib_config.found_libs.len() != 1 {
-            //     panic!(format!("found {} libs, expecting 1", lib_config.found_libs.len()));
-            // }
-
-            // fs::copy(Path::new(&lib_config.found_libs[0]),
-            //          Path::new(&env::var_os("OUT_DIR").unwrap()).join("zmq.lib"))
-            //         .expect("Could not copy libzmq.lib to OUT_DIR/zmq.lib");
-
-            // if you remove this then the build script for the zmq crate will not link
-            // if lib_config.is_static {
-            //     println!("cargo:rustc-link-lib=static=zmq");
-            // }
-
-            // println!("cargo:rustc-link-search=native={}",
-            //          env::var("OUT_DIR").unwrap());
-
-            // // emit a rustc-link-search line without native= so build.rs zmq crate
-            // // can find libzmq.dll at build time. workaround for cargo bug
-            // // https://github.com/rust-lang/cargo/issues/3957
-            // for path in lib_config.dll_paths {
-            //     println!("cargo:rustc-link-search={}", path.to_str().unwrap());
-            // }
-
-            // for (key, value) in env::vars() {
-            //     println!("{}: {}", key, value);
-            // }
-            // panic!();
-
             true
         }
     }
